2d706000f9ce4db3c934a2ae60ae780b9287a4b0
m561247898 <m561247898@gmail.com>
2023-08-13 21:50:56 +0800

    update alhpa, beta

--- quiz1/alpha/main.c
+++ quiz1/alpha/main.c
@@ -264,8 +264,10 @@ void st_remove(struct st_node **root, struct st_node *del)
         if (del == *root)
             *root = least;
 
-        AAAA;
-        BBBB;
+        // AAAA;
+        st_replace_right(del, least);
+        // BBBB;
+        st_update(root, st_right(least));
         return;
     }
 
@@ -274,8 +276,10 @@ void st_remove(struct st_node **root, struct st_node *del)
         if (del == *root)
             *root = most;
 
-        CCCC;
-        DDDD;
+        // CCCC;
+        st_replace_left(del, most);
+        // DDDD;
+        st_update(root, st_left(most));
         return;
     }
 
@@ -292,7 +296,8 @@ void st_remove(struct st_node **root, struct st_node *del)
     else
         st_right(parent) = 0;
 
-    EEEE;
+    // EEEE;
+    st_update(root, parent);
 }
 
 /* Test program */
@@ -407,13 +412,14 @@ static void __treeint_dump(struct st_node *n, int depth)
 {
     if (!n)
         return;
-
-    __treeint_dump(FFFF, depth + 1);
+    // FFFF
+    __treeint_dump(st_left(n), depth + 1);
 
     struct treeint *v = treeint_entry(n);
     printf("%d\n", v->value);
 
-    __treeint_dump(GGGG, depth + 1);
+    // GGGG
+    __treeint_dump(st_right(n), depth + 1);
 }
 
 void treeint_dump()
--- quiz1/beta/main.c
+++ quiz1/beta/main.c
@@ -1,15 +1,19 @@
 #include <stdint.h>
-
+#include <stdio.h>
 static inline uintptr_t align_up(uintptr_t sz, size_t alignment)
 {
     uintptr_t mask = alignment - 1;
     if ((alignment & mask) == 0) { /* power of two? */
-        return MMMM;
+        return (sz + mask) & ~mask;
     }
+    printf("~mask = %ld\n", ~mask);
     return (((sz + mask) / alignment) * alignment);
 }
 
 int main()
 {
+    printf("120, 4 : %ld\n", align_up(5, 4));
+    printf("124, 4 : %ld\n", align_up(124, 4));
+
     return 0;
 }
